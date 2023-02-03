e17f01a2414cb018296bee65cf2e0d49bcbf226f
ItZmEkAtHiR <64773152+ItZmEkAtHiR@users.noreply.github.com>
2023-02-04 02:03:06 +0530

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import './style.css';
+import Select from 'react-select';
+import './style.scss';
 const App = () => {
   const [spaceAvailable, setSpaceAvailable] = useState(true);
+  const country = [
+    { value: 'india', label: 'india' },
+    { value: 'china', label: 'china' },
+    { value: 'china', label: 'china' },
+    { value: 'china', label: 'china' },
+    { value: 'china', label: 'china' },
+    { value: 'china', label: 'china' },
+    // { value: 'china', label: 'china' },
+    // { value: 'china', label: 'china' },
+  ];
+  let margin = 0;
 
   useEffect(() => {
     // Check if there is enough space at the bottom of the viewport
     const checkSpace = () => {
-      const element = document.querySelector('.select-container');
+      const element = document.querySelector('.select');
       const elementRect = element.getBoundingClientRect();
       const viewportHeight = window.innerHeight;
-      const optionElement = document.querySelector('option');
-      const optionHeight = optionElement.offsetHeight;
+      const optionHeight = element.offsetHeight || 0;
       const spaceAtBottom = viewportHeight - elementRect.bottom;
-
-      if (spaceAtBottom < optionHeight) {
+      console.log({ spaceAtBottom });
+      console.log(optionHeight * country.length);
+      margin = optionHeight * country.length;
+      if (spaceAtBottom < margin) {
         setSpaceAvailable(false);
       } else {
         setSpaceAvailable(true);
@@ -27,14 +40,26 @@ const App = () => {
     // Remove the event listener on component unmount
     return () => window.removeEventListener('resize', checkSpace);
   }, []);
-
+  const ddStyles = {
+    control: (styles) => ({
+      ...styles,
+      cursor: 'pointer',
+      // margin: spaceAvailable ? '0' : `-${margin}px`,
+    }),
+  };
   return (
     <div className="select-container">
-      <select className={`select ${!spaceAvailable && 'upsideDown'}`}>
+      {/* <select className={`select ${!spaceAvailable && 'upsideDown'}`}>
         <option value="india">India</option>
         <option value="china">China</option>
         <option value="usa">USA</option>
-      </select>
+      </select> */}
+      <div className={`select ${!spaceAvailable && 'upsideDown'}`}>
+        <Select
+          options={country}
+          menuPlacement={!spaceAvailable ? 'top' : 'bottom'}
+        />
+      </div>
     </div>
   );
 };
